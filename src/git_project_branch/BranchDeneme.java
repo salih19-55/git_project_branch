8a2019d4f76172d16939398aeaf38041d4969b44
Salih1-55 <buenosaires.sy19@gmail.com>
2020-08-16 13:29:53 -0300

    ilk commit from master

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -3,6 +3,10 @@ package git_project_branch;
 public class BranchDeneme {
 
 	public static void main(String[] args) {
+	
+		System.out.println("16;28 - master branchten ilk commit");
+		
+		
 		
 	}
 
